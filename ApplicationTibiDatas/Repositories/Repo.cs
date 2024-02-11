27bd48205edf18f4e6a6f4a73215524fc56e6bc4
sanna-hansson <sanna-hansson@live.com>
2024-02-11 16:22:53 +0100

    Tentity crud created

---
+++ ApplicationTibiDatas/Repositories/Repo.cs
@@ -0,0 +1,50 @@
+﻿
+
+using ApplicationTibiDatas.Contexts;
+using System.Linq.Expressions;
+
+namespace ApplicationTibiDatas.Repositories;
+
+internal class Repo<TEntity> where TEntity : class
+{
+    private readonly DataContext _context;
+
+    public Repo(DataContext context)
+    {
+        _context = context;
+    }
+
+    public TEntity Create (TEntity entity)
+    {
+        _context.Set<TEntity>().Add(entity);
+        _context.SaveChanges();
+        return entity;
+    }
+
+    public IEnumerable <TEntity> GetEverything()
+    {
+        return _context.Set<TEntity>().ToList();
+        
+    }
+    public TEntity Get (Expression<Func<TEntity, bool >> expression) 
+    {
+        var entity = _context.Set<TEntity>().FirstOrDefault(expression);
+        return entity!;
+    }
+
+    public TEntity Update (Expression<Func<TEntity, bool>> expression ,TEntity entity)
+    {
+        var entityToUpdate = _context.Set<TEntity>().FirstOrDefault(expression);
+        _context.Entry(entityToUpdate!).CurrentValues.SetValues(entity);
+
+        _context.SaveChanges();
+
+        return entityToUpdate!;
+    }
+    public void Delete(Expression<Func<TEntity, bool>> expression)
+    {
+        var entity = _context.Set<TEntity>().FirstOrDefault(expression);
+        _context.Remove(entity!);
+        _context.SaveChanges();
+    }
+}
