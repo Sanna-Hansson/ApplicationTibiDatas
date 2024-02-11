ae84aa6e859b3996cd0b759a41328e32f84bcbe3
sanna-hansson <sanna-hansson@live.com>
2024-02-11 14:16:35 +0100

    started datacontext

---
+++ ApplicationTibiDatas/Contexts/DataContext.cs
@@ -0,0 +1,26 @@
+﻿
+
+using ApplicationTibiDatas.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApplicationTibiDatas.Contexts;
+
+internal class DataContext : DbContext
+{
+    public DataContext(DbContextOptions <DataContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet <AdressEntity> Adresses { get; set; }
+
+    public DbSet <CategoryEntity> Categories { get; set; }
+
+    public DbSet <CustomerEntity> Customers { get; set; }
+
+    public DbSet <ProductEntity> Products { get; set; }
+
+    public DbSet <RoleEntity> Roles { get; set; }
+
+
+}
